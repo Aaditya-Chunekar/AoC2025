36bc3d7a543d4d45e28ae05cdd0a6f37c57efc56
Aaditya-Chunekar <aadityachunekar@gmail.com>
2026-07-05 12:13:11 +0530

    dayOne

---
+++ README.md
@@ -0,0 +1,3 @@
+AoC2025
+___
+My solutions to AoC2025
---
+++ day1.py
@@ -0,0 +1,25 @@
+b,l,r=50,0,99
+ans=0
+with open("day1.ip","r") as f:
+    for line in f:
+        # print('-'*10)
+        v=int(line[1:])
+        # print(line, v)
+        if(line[0]=="L"):
+            b-=v
+            if(b<l):
+                b%=100
+                
+        else:
+            b+=v
+            if(b>r):
+                b%=100
+            break
+        if(b==0):
+           ans+=1
+print(ans)
+
+'''
+ after the granting of star, 
+ git add .; git commit -m "dayOne"; git push
+ '''
