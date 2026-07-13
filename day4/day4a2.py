45441fd2e081f6780413dc4a0aca8dfffb3cfe07
Aaditya-Chunekar <aadityachunekar@gmail.com>
2026-07-13 08:05:51 +0530

    dayFourPartTwo

--- day4/day4a2.py
+++ day4/day4a2.py
@@ -1,26 +1,25 @@
 t,ip=10,137
-def partOne(fileName, h):
+#should take dept over fileName here - dept should be modified in the counting loop 
+def partOne(dept, h):
     tt=[]
+    DIRS = ((1,0), (0,1), (1,1), (1,-1))
     for i in range(h):
         tt.append([0]*h)
     # print(tt)
-    dept=[]
+    
     # perRow=[5,1,1,0,2,0,0,1,0,3]
     ans=0
-    with open(fileName,"r") as f:
-        for line in f:
-            dept.append(line[:-1])
+    
     # print(dept)
     for i in range(0,h):
         for j in range(0,h):
             if dept[i][j]=='@':
                 # print("-"*10)
                 # print(i,j,"\n---\n")
-                dr=[1,0,1, 1]
-                dc=[0,1,1,-1]
-                for k in range(len(dc)):
-                    nr=i+dr[k]
-                    nc=j+dc[k]
+                
+                for dr,dc in DIRS:
+                    nr=i+dr
+                    nc=j+dc
                     if(0<=nr<h and 0<=nc<h):
                         if(dept[nr][nc]=='@'):
                             tt[i][j]+=1
@@ -28,10 +27,30 @@ def partOne(fileName, h):
                             tt[nr][nc]+=1
             else:
                 tt[i][j]=4
-    for i in tt:
-        ans+=sum(1 for x in i if x<4)
-    return ans
-print(partOne("day4.ip",ip))
+    for i in range(h):
+        for j in range(h):
+            if(tt[i][j]<4):
+                dept[i][j]='.'
+                ans+=1
+    return ans, dept
+
+dept=[]
+ans=1
+with open("day4.ip","r") as f:
+        for line in f:
+            dept.append(list(line[:-1]))
+# partOne
+ansOne,dept=partOne(dept,ip)
+print(ansOne)
+
+# partTwo
+ansTwo=0
+while(ans):
+    ans=0
+    ans,dept=partOne(dept,ip)
+    # print(ans,"-"*10)
+    ansTwo+=ans
+print(ansTwo+ansOne)
 
 
 
